13d6838d8ab9230b805383a494f52f524bc3507e
Ij888 <ij.ofili@gmail.com>
2016-02-06 18:00:16 +0000

    added entry

---
+++ meteorApp1/startup.js
@@ -0,0 +1,13 @@
+if (Meteor.isServer){
+	Meteor.startup(function(){
+		if (Images.find().count() == 0){
+			Images.insert(
+				{img_src:"mtr.jpg", img_alt:"a meteor"}
+			);
+		}//end of if #hasNoImages
+	});
+}
+
+if (meteor.isClient){
+	
+}
